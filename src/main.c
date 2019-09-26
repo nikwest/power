8c0a6db33309883e11741c98d9cae393e7c0f23e
Dominik Westner <nikwest@github.com>
2019-09-26 10:27:24 +0200

    initial commit

--- src/main.c
+++ src/main.c
@@ -16,24 +16,36 @@
  */
 
 #include "mgos.h"
+#include "mgos_config.h"
+#include "mgos_rpc.h"
 
-static void timer_cb(void *arg) {
+#include "power.h"
+#include "rpc.h"
+#include "adc.h"
+
+static void timer_cb(void *data) {
   static bool s_tick_tock = false;
   LOG(LL_INFO,
       ("%s uptime: %.2lf, RAM: %lu, %lu free", (s_tick_tock ? "Tick" : "Tock"),
        mgos_uptime(), (unsigned long) mgos_get_heap_size(),
        (unsigned long) mgos_get_free_heap_size()));
   s_tick_tock = !s_tick_tock;
-#ifdef LED_PIN
-  mgos_gpio_toggle(LED_PIN);
-#endif
-  (void) arg;
+  #ifdef LED_PIN
+    mgos_gpio_toggle(LED_PIN);
+  #endif
+
 }
 
 enum mgos_app_init_result mgos_app_init(void) {
-#ifdef LED_PIN
-  mgos_gpio_setup_output(LED_PIN, 0);
-#endif
-  mgos_set_timer(1000 /* ms */, MGOS_TIMER_REPEAT, timer_cb, NULL);
+  #ifdef LED_PIN
+    mgos_gpio_setup_output(LED_PIN, 0);
+  #endif
+
+  mgos_set_timer(10000 /* ms */, MGOS_TIMER_REPEAT, timer_cb, NULL);
+
+  adc_init();
+  power_init();
+  rpc_init();
+  
   return MGOS_APP_INIT_SUCCESS;
 }
